de8c88a341b60fe23ff5a60d57c025b541560271
Anna Cuomo <annacuomo92@gmail.com>
2023-10-17 09:36:47 +1100

    Create summarise_by_celltype.R

---
+++ Rscripts_downstream/summarise_by_celltype.R
@@ -0,0 +1,28 @@
+library(data.table)
+
+mydir = "/share/ScratchGeneral/anncuo/OneK1K/saige_eqtl/from_wei/"
+
+# change, have this as an argument
+celltype = "B_IN"
+ct_dir = paste0(mydir, "cis_", celltype,"/")
+ct_files = list.files(ct_dir, pattern = "singleVar.txt")
+
+# combine results across genes
+file_df_list = list()
+for (file in ct_files){
+    filename = paste0(ct_dir,file)
+    df_curr = as.data.frame(data.table:::fread(filename, header = T, stringsAsFactors = FALSE))
+    df_curr[["gene"]] = gsub("_.*","",file)
+    file_df_list[[file]] = df_curr
+}
+df = rbindlist(file_df_list, fill=TRUE)
+
+# save all results
+new_file_all = paste0(ct_dir,celltype,"_all_results_cis.txt")
+data.table:::fwrite(df,new_file_all)
+
+# save top SNP per gene only
+df = df[order(df$p.value),]
+df0 = df[-which(duplicated(df$gene)),]
+new_file_leads = paste0(ct_dir,celltype,"_top_snp_cis.txt")
+data.table:::fwrite(df0,new_file_leads)
