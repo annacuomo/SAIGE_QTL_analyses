e99124a2873a6c35a02932a4507c504bb1d2e59d
Anna Cuomo <annacuomo92@gmail.com>
2023-12-18 13:09:45 +1100

    Update chr_specific_saige_qtl_results.R

--- SMR/chr_specific_saige_qtl_results.R
+++ SMR/chr_specific_saige_qtl_results.R
@@ -1,29 +1,20 @@
 library(data.table)
 
 results_dir = "/share/ScratchGeneral/anncuo/OneK1K/saige_eqtl/from_wei/"
-unique_egenes_dir = "/share/ScratchGeneral/anncuo/OneK1K/saige_eqtl/genes/egenes_saigeqtl_not_tensorqtl/"
+out_dir_all = "/share/ScratchGeneral/anncuo/OneK1K/saige_eqtl/ct_chrom_summary_stats/"
 
 celltypes = c("B_IN","B_Mem","CD4_NC", "CD4_ET", "CD4_SOX4", "CD8_NC", "CD8_ET",
               "CD8_S100B", "DC", "Mono_C", "Mono_NC", "NK_R", "NK", "Plasma")
 
 for (celltype in celltypes){
-    ct_results_dir = paste0(results_dir,"cis_",celltype,"/")
-    ct_results_files = list.files(ct_results_dir, pattern="singleVar")
-    select_genes_file = paste0(unique_egenes_dir,celltype,".csv")
-    select_genes_df = as.data.frame(data.table:::fread(select_genes_file))
-    for (chrom in 1:22){
-        genes = select_genes_df[select_genes_df$seqid == chrom, "gene_name"]
-        df_list = list()
-        for (file in ct_results_files){
-            gene = gsub("_.*","",file)
-            if (!(gene %in% genes)){next}
-            filename = paste0(ct_results_dir,file)
-            df = as.data.frame(data.table:::fread(filename))
-            df$gene = gene
-            df_list[[file]] = df
-        }
-        df_chrom_ct = rbindlist(df_list)
-        out_file = paste0(unique_egenes_dir,"ct_chrom_summary_stats/",celltype,"/",celltype,"_chr",chrom,".txt")
-        data.table:::fwrite(df_chrom_ct, out_file)
-    }
+  ct_results_dir = paste0(results_dir,"cis_",celltype,"/")
+  ct_results_file = paste0(ct_results_dir, celltype, "_all_results_cis.txt.gz")
+  df = read.csv(gzfile(ct_results_file))
+  for (chrom in 1:22){
+    df_chrom_ct = df[df$CHR==chrom,]
+    out_dir = paste0(out_dir,celltype,"/")
+    if (!dir.exists(out_dir)) {dir.create(out_dir)}
+    out_file = paste0(out_dir,celltype,"_chr",chrom,".txt")
+    data.table:::fwrite(df_chrom_ct, out_file)
+  }
 }
