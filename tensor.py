a3b2f341851fb6de1cf919b8826e7a16303803c5
Anna Cuomo <annacuomo92@gmail.com>
2024-01-02 21:58:07 +1100

    20pct

--- tensor.py
+++ tensor.py
@@ -9,9 +9,9 @@ chr_num = 22
 
 # define paths to data
 plink_prefix_path = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/plink_files/plink_chr{chr_num}"
-expression_bed = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/input/Jan24/tensorqtl/CD4_NC_50pct_subset/chr{chr_num}.bed.gz"
-covariates_file = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/input/Jan24/tensorqtl/CD4_NC_50pct_subset/chr{chr_num}_covs.txt"
-path = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/output/tensorqtl/CD4_NC_50pct_subset"
+expression_bed = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/input/Jan24/tensorqtl/CD4_NC_20pct_subset/chr{chr_num}.bed.gz"
+covariates_file = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/input/Jan24/tensorqtl/CD4_NC_20pct_subset/chr{chr_num}_covs.txt"
+path = f"/directflow/SCCGGroupShare/projects/anncuo/OneK1K/output/tensorqtl/CD4_NC_20pct_subset"
 prefix = path
 if os.path.exists(path) == False:
     os.makedirs(path)
