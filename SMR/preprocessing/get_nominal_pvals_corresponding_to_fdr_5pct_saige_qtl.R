d62eee87b173549a55244297c6b6493ade6524a8
Anna Cuomo <annacuomo92@gmail.com>
2024-02-27 17:16:10 +1100

    Update get_nominal_pvals_corresponding_to_fdr_5pct_saige_qtl.R

--- SMR/preprocessing/get_nominal_pvals_corresponding_to_fdr_5pct_saige_qtl.R
+++ SMR/preprocessing/get_nominal_pvals_corresponding_to_fdr_5pct_saige_qtl.R
@@ -1,1 +1,17 @@
 saige_dir = "/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/from_wei/Feb24/"
+
+# unique cell types
+celltypes = unique(df$celltype)
+
+# loop over cell types
+for (celltype in celltypes){
+    df1 = as.data.frame(df[df$celltype == celltype,])
+    genes = df1[df1$qval==max(df1[df1$qval<0.05,]$qval),]$gene
+    max_pv = 0
+    for (gene in genes){
+        file = paste0(results_dir,celltype,"_count_saigeqtl_",gene,"_cis_single.MAFgt0.05.txt")
+        df2 = fread(file)
+        max_pv = max(max_pv, min(df2$p.value))
+    }
+    print(c(celltype, max_pv))
+}
