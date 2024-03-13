620f07032449bcec4b5882423ce679bea87e539b
Anna Cuomo <annacuomo92@gmail.com>
2024-03-14 09:49:36 +1100

    Update make_subset_pseudobulk.R

--- preprocessing/make_subset_pseudobulk.R
+++ preprocessing/make_subset_pseudobulk.R
@@ -1,3 +1,5 @@
 library(data.table)
 
 sct_dir = "/directflow/SCCGGroupShare/projects/anncuo/OneK1K/saige_eqtl/input/Sept23/SCT/"
+
+output_dir = "/directflow/SCCGGroupShare/projects/anncuo/OneK1K_from_ScratchGeneral/OneK1K/saige_eqtl/input/Sept23/SCT/"
